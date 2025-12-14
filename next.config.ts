83568634043e6b6bce6603e49bf496a0d5a456b0
Langning Chen <chenlangning2009@outlook.com>
2025-12-14 20:34:29 +0800

    fix: remove external package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@octokit/rest'],
   webpack: (config, { dev, isServer }) => {
     if (dev && !isServer) {
       config.resolve.fallback = {
